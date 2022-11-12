ad48bec243eed125e7eb3e03cd61b8d89f02e224
Patrick Stadler <patrick.stadler01@gmail.com>
2022-11-12 16:27:50 +0100

    changed landingpage

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           primarySwatch: Colors.red,
           fontFamily: GoogleFonts.inter().fontFamily),
-      home: DashboardPage_Copy(),
+      home: LoginPage(),
     );
   }
 }
